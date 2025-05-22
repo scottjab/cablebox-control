d354542267ffd918115e09418666ccd9c852d81c
James Scott <jim@tailscale.com>
2025-05-22 08:58:10 -0700

    Let's try this

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Cablebox control application";
+  description = "Cablebox Control Service";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -7,37 +7,53 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      perSystem = system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          package = pkgs.callPackage ./nix/packages {
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = {
+          cablebox-control = pkgs.buildGoModule {
+            pname = "cablebox-control";
+            version = "0.1.0";
             src = ./.;
+            vendorHash = null; # This will be set automatically on first build
           };
+          default = self.packages.${system}.cablebox-control;
+        };
+      }
+    ) // {
+      nixosModules.cablebox-control = { config, lib, pkgs, ... }:
+        with lib;
+        let
+          cfg = config.services.cablebox-control;
         in
         {
-          packages = {
-            default = package;
-            cablebox-control = package;
+          options.services.cablebox-control = {
+            enable = mkEnableOption "Cablebox Control Service";
+            package = mkOption {
+              type = types.package;
+              default = self.packages.${pkgs.system}.cablebox-control;
+              description = "The cablebox-control package to use.";
+            };
           };
 
-          devShells.default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              go
-              gopls
-              gotools
-            ];
+          config = mkIf cfg.enable {
+            systemd.services.cablebox-control = {
+              description = "Cablebox Control Service";
+              wantedBy = [ "multi-user.target" ];
+              after = [ "network.target" ];
+              serviceConfig = {
+                ExecStart = "${cfg.package}/bin/cablebox-control";
+                Restart = "always";
+                RestartSec = "10";
+                DynamicUser = true;
+                ProtectSystem = "strict";
+                ProtectHome = true;
+                NoNewPrivileges = true;
+              };
+            };
           };
         };
-    in
-    flake-utils.lib.eachSystem supportedSystems perSystem
-    // {
-      nixosModules.default = { config, lib, pkgs, ... }: import ./nix/modules/service.nix { inherit config lib pkgs; };
     };
-}
+} 
